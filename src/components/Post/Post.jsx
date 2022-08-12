c0b08d576f8668c2d7b3da1b1b0edfb8cb4b4c31
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-08-12 15:20:57 -0300

    creating avatar component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../Avatar/Avatar';
 import { Comment } from '../Comment/Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/79596096?v=4" />
+          <Avatar src="https://avatars.githubusercontent.com/u/79596096?v=4" />
           <div className={styles.authorInfo}>
             <strong>Eugênio Júnior</strong>
             <span>Web Developer</span>
