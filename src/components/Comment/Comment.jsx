16248229084e625c8e0fc571582361bbaf802339
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-08-12 13:42:55 -0300

    creating comment component

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/79596096?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Eugênio Júnior</strong>
+              <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Cerca de 2h atrás</time>
+            </div>
+
+            <button title='Deletar Comentário'>
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
