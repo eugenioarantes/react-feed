14bd1708c07d298e9a1f5a1faeb8e4cbd22dc67f
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-08-12 10:38:00 -0300

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Header } from './components/Header/Header';
+import { Post } from './components/Post/Post';
+import { Sidebar } from './components/SideBar/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Eugenio Junior"
+            content="Conteudo 1"
+          />
+
+          <Post 
+            author="Eugenio Arantes"
+            content="Conteudo 2"
+          />
+          
+        </main>
+      </div>
+    </div>
+  )
+}
