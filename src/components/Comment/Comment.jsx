3989f34e5bea89a49da0f61a04b70f4d9f75af4b
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-08-18 14:58:51 -0300

    removing comment

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from '../Avatar/Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  };
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/eugenioarantes.png" alt="" />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
               <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Cerca de 2h atrás</time>
             </div>
 
-            <button title='Deletar Comentário'>
+            <button onClick={handleDeleteComment} title='Deletar Comentário'>
               <Trash size={24}/>
             </button>
           </header>
